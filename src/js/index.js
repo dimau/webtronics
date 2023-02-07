168fbd5d81162f631319a249b79b81af5fd890de
Dmitrii <dimau777@gmail.com>
2023-02-07 17:50:52 +0400

    Section FAQ is ready

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,11 @@ import "../styles/promo.scss";
 import "../styles/about.scss";
 import "../styles/technologies.scss";
 import "../styles/steps.scss";
+import "../styles/accordion.scss";
+import "../styles/faq.scss";
+
+import { Accordion } from "./accordion";
+
+new Accordion(document.querySelector(".accordion"), {
+  alwaysOpen: false,
+});
